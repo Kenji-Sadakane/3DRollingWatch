172b2dae6ceaa39cd311258fabc3800cfbdc205f
Kenji.Sadakane <SADA.732738686237@GMAIL.COM>
2017-01-06 23:42:56 +0900

    add xwalk core library

--- app/src/main/java/com/keepingrack/a3drollingwatch/MainActivity.java
+++ app/src/main/java/com/keepingrack/a3drollingwatch/MainActivity.java
@@ -1,13 +1,19 @@
 package com.keepingrack.a3drollingwatch;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.xwalk.core.XWalkView;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        XWalkView mXWalkView = (XWalkView) findViewById(R.id.webView);
+        mXWalkView.load("file:///android_asset/index.html", null);
     }
 }
